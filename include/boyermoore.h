374af54ef8e89fe249e41b667790bac88c9f1237
Ludvig Sundström <lud.sund@gmail.com>
2018-04-13 15:53:31 +0200

    Add boyermoore

---
+++ include/boyermoore.h
@@ -0,0 +1,12 @@
+
+#ifndef BOYERMOORE_H
+#define BOYERMOORE_H
+
+#include "types.h"
+
+Bool bmhsearch(Uchar *text, Uint textlen, Uchar *pattern, Uchar *patternright);
+
+
+Bool naive_search(Uchar *text, Uint textlen, Uchar *pattern, Uchar *patternright);
+
+#endif
